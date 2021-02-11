75cf448f1a4e721f6de7d0af86ad0462c0e2d839
Prashasthi K Kanchan <75254746+prashasthikanchan@users.noreply.github.com>
2021-02-11 17:31:46 +0530

    Update two_fractions.c

--- two_fractions.c
+++ two_fractions.c
@@ -22,9 +22,9 @@ float sum;
 sum = ((f1.n/f1.d)+(f2.n/f2.d));
 return sum;
 }
-void output(float sum_of_num)
+void output(fraction f1, fraction f2, float sum_of_num)
 {
-printf("the sum of the given fractions is %f\n",sum_of_num);
+printf("the sum of the given fractions %d/%d and %d/%d is %f\n",f1.n,f1.d,f2.n,f2.d,sum_of_num);
 }
 float main()
 {
@@ -33,6 +33,6 @@ fraction f1,f2;
 f1 = input();
 f2 = input();
 sum_of_num = compute(f1,f2);
-output(sum_of_num);
+output(f1,f2,sum_of_num);
 return 0;
 }
