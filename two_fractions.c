19d5a567c7c72fd97c983d930473f923f051c4fc
Prashasthi K Kanchan <75254746+prashasthikanchan@users.noreply.github.com>
2021-02-13 14:35:22 +0530

    Update two_fractions.c

--- two_fractions.c
+++ two_fractions.c
@@ -15,25 +15,27 @@ printf("enter the value of denominator:");
 scanf("%f",&f.d);
 return f;
 }
-float compute(fraction f1, fraction f2)
+float compute_n(fraction f1, fraction f2)
 {
-float sum, fff1, fff2;
-sum = ((f1.n/f1.d)+(f2.n/f2.d));
-
-return sum;
+float sum_num;
+sum_num = (f1.n*f2.d)+(f2.n*f1.d);
+return sum_num;
 }
-void output(fraction f1, fraction f2, float sum_of_num)
+float compute_d(fraction f1, fraction f2)
 {
-printf("the sum of the given fractions %f/%f and %f/%f is %f\n",f1.n,f1.d,f2.n,f2.d,sum_of_num);
+float sum_den;
+sum_den = f1.d*f2.d;
+return sum_den;
 }
 float main()
 {
-float sum_of_num;
+float sum_of_num,sum_of_den;
 fraction f1,f2;
 f1 = input();
 f2 = input();
-sum_of_num = compute(f1,f2);
-output(f1,f2,sum_of_num);
+sum_of_num = compute_n(f1,f2);
+sum_of_den = compute_d(f1,f2);
+printf("the sum of the given fractions %f/%f and %f/%f is %f/%f\n",f1.n,f1.d,f2.n,f2.d,sum_of_num,sum_of_den);
+
 return 0;
 }
-
