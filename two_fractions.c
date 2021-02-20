3432d6fb0689438dad4785e7e885fcab833f1d6d
Prashasthi K Kanchan <75254746+prashasthikanchan@users.noreply.github.com>
2021-02-20 21:52:07 +0530

    Update two_fractions.c

--- two_fractions.c
+++ two_fractions.c
@@ -1,40 +1,41 @@
 #include<stdio.h>
-#include<math.h>
 struct value
 {
-int n;
-int d;
+    int n;
+    int d;
 };
 typedef struct value fraction;
 fraction input()
 {
-fraction f;
-printf("enter the value of numerator:");
-scanf("%d",&f.n);
-printf("enter the value of denominator:");
-scanf("%d",&f.d);
-return f;
+    fraction f;
+    printf("enter the value of numerator:");
+    scanf("%d",&f.n);
+    printf("enter the value of denominator:");
+    scanf("%d",&f.d);
+    return f;
 }
-int compute_n(fraction f1, fraction f2)
+fraction compute(fraction f1, fraction f2)
 {
-int sum_num;
-sum_num = (f1.n*f2.d)+(f2.n*f1.d);
-return sum_num;
+    fraction sum;
+    int sum_num;
+    int sum_den;
+    sum.n = (f1.n*f2.d)+(f2.n*f1.d);
+    sum.d = f1.d*f2.d;
+    return sum;
 }
-int compute_d(fraction f1, fraction f2)
+void output(fraction f1, fraction f2, fraction sum)
 {
-int sum_den;
-sum_den = f1.d*f2.d;
-return sum_den;
+    printf("the sum of the given fractions %d/%d and %d/%d is %d/%d\n",f1.n,f1.d,f2.n,f2.d,sum.n,sum.d);
 }
-int main()
+float main()
 {
-int sum_of_num,sum_of_den;
-fraction f1,f2;
-f1 = input();
-f2 = input();
-sum_of_num = compute_n(f1,f2);
-sum_of_den = compute_d(f1,f2);
-printf("the sum of the given fractions %d/%d and %d/%d is %d/%d\n",f1.n,f1.d,f2.n,f2.d,sum_of_num,sum_of_den);
-return 0;
+    fraction sum;
+    fraction f1,f2;
+    f1 = input();
+    f2 = input();
+    sum = compute(f1,f2);
+    output(f1,f2,sum);
+    return 0;
 }
+
+
